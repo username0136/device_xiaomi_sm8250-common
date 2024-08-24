2a6beb61d58eb79724594edfbd2cd6a949bc11f6
Sugakesshaa <sugakesshaa@pixelos.net>
2024-08-24 22:58:06 +0530

    sm8250-common: Move to pixel power HAL
    Change-Id: I09b2f35fe113805e022874e9c82214ff019dd7b9

--- kona.mk
+++ kona.mk
@@ -428,10 +428,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 # PocketMode
 PRODUCT_PACKAGES += \
     PocketMode
@@ -441,9 +437,14 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client \
-    android.hardware.power@1.2.vendor
+    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power@1.2.vendor \
+    libqti-perfd-client
+
+# Enable adpf cpu hint session for SurfaceFlinger and HWUI
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.enable_adpf_cpu_hint=true \
+    debug.hwui.use_hint_manager=true
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
@@ -519,7 +520,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
