2688b4e5f5bb4d0f2c457f1440a3a646607e8268
lahaina <nattolecats@gmail.com>
2024-09-12 23:02:58 +0530

    sm8250-common: Link libwvhidl against libcrypto from vndk 33 * It depends on symbol CBS_init which is removed from the latest libcrypto.
    Change-Id: I4fb1bf4502590ea0dab13d5b930acc178f831f64
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- kona.mk
+++ kona.mk
@@ -236,7 +236,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto-v33
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
