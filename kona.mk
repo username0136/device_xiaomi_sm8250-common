69390d412c5ff9619346a3c276dc63a6f998db65
Bruno Martins <bgcngm@gmail.com>
2024-09-21 16:05:53 +0530

    sm8250-common: Use common libqti-perfd-client

--- kona.mk
+++ kona.mk
@@ -428,10 +428,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    libqti-perfd-client
-
 # PocketMode
 PRODUCT_PACKAGES += \
     PocketMode
@@ -442,6 +438,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
+    libqti-perfd-client \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
@@ -518,6 +515,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
